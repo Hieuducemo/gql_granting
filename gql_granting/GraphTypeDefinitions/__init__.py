56254ebb11fee3e28d201963b88db6f6a6c5608d
Hieuducemo <115462421+Hieuducemo@users.noreply.github.com>
2023-11-07 22:39:49 +0100

    schema update

--- gql_granting/GraphTypeDefinitions/__init__.py
+++ gql_granting/GraphTypeDefinitions/__init__.py
@@ -2,6 +2,9 @@ import strawberry
 
 from .query import Query
 from .mutation import Mutation
+from .externals import UserGQLModel,GroupGQLModel
 
-schema = strawberry.federation.Schema(query=Query, mutation=Mutation)
+schema = strawberry.federation.Schema(query=Query, mutation=Mutation,types=(
+    UserGQLModel,GroupGQLModel
+))
 # schema = strawberry.federation.Schema(query=Query)
